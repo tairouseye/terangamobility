b779c196b22c99a128f9f30f3bfdbaafed6340e3
tairouseye <gesprosn@gmail.com>
2026-07-17 21:18:24 +0000

    Centre de notifications + cloche badge non-lus

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "304929708" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "671286891" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
